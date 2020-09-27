039452a00c87297503e2d554ef8ca373811c8603
andryushque <andrew.ustinov23@gmail.com>
2020-09-27 12:34:27 +0300

    Create: mobile menu media

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
-  // js code
+  /*=== Mobile Menu ===*/
+  // Show and hide navigation menu
+  const mmenuButtonOpen = $(".mmenubtn--open");
+  const mmenuButtonClose = $(".mmenubtn--close");
+  const mobileMenu = $(".mmenu");
+  mmenuButtonOpen.on("click", function () {
+    mobileMenu.addClass("mmenu--visible");
+  });
+  mmenuButtonClose.on("click", function () {
+    mobileMenu.removeClass("mmenu--visible");
+  });
+
+  // Close mobile menu on wide screens
+  $(window).resize(function () {
+    if (screen.width > 991) {
+      mobileMenu.removeClass("mmenu--visible");
+    }
+  });
 });
