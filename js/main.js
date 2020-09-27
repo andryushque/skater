1cbcf80dd53d6879ec027341fc27cbd1a4c2066e
andryushque <andrew.ustinov23@gmail.com>
2020-09-27 15:10:43 +0300

    Create: dropdown menu

--- js/main.js
+++ js/main.js
@@ -4,11 +4,24 @@ $(document).ready(function () {
   const mmenuButtonOpen = $(".mmenubtn--open");
   const mmenuButtonClose = $(".mmenubtn--close");
   const mobileMenu = $(".mmenu");
+
   mmenuButtonOpen.on("click", function () {
     mobileMenu.addClass("mmenu--visible");
   });
   mmenuButtonClose.on("click", function () {
     mobileMenu.removeClass("mmenu--visible");
+  });
+
+  const mobileMmenu1 = $(".mmenu-dropdown1 > .mmenu-dropdown1__link");
+  mobileMmenu1.on("click", function () {
+    mobileMmenu1.toggleClass("active");
+    $(".mmenu-dropdown1__list").slideToggle(300);
+  });
+
+  const mobileMenu2 = $(".mmenu-dropdown2 > .mmenu-dropdown2__link");
+  mobileMenu2.on("click", function () {
+    mobileMenu2.toggleClass("active");
+    $(".mmenu-dropdown2__list").slideToggle(300);
   });
 
   // Close mobile menu on wide screens
